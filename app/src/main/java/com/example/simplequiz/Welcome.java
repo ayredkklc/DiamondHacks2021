77c81b6eb452cb2dad1207d35025e56849eb21fc
anpavao <anpavao@ncsu.edu>
2021-03-27 21:54:05 -0400

    Adding fun facts page and changing colors

--- app/src/main/java/com/example/simplequiz/Welcome.java
+++ app/src/main/java/com/example/simplequiz/Welcome.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.os.Bundle;
 
 public class Welcome extends AppCompatActivity {
-    Button start;
+    Button start, facts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +23,15 @@ public class Welcome extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        facts = (Button)findViewById(R.id.btn_facts);
+        facts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Welcome.this, FactsActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
